84a316e5caa486d0cebb002825b717b9aadefe79
Dũng Nguyễn <nguyenquangdung2309@gmail.com>
2025-04-26 05:31:42 +0700

    fix connect DB

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB error:", err));
 
